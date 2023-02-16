f068d495878313f749b063243f5615a5723c4180
Alex-98jeweler <zubakin.alexandr98@gmail.com>
2023-02-16 12:32:42 +0700

    add get item view

--- items/views.py
+++ items/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django import views
 
-# Create your views here.
+from .models import Items
+
+
+
+
+class ItemsView(views.View):
+
+    def get(request, pk):
+        item = Items.objects.get(pk=pk)
+        context = {}
+        context['name'] = item.name
+        context['description'] = item.description
+        context['price'] = item.price
+        return render(request, 'item_detail.html', context)
