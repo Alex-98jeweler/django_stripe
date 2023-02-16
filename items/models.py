d56b66f43d31a9dcfacf9023e16018954423a3d7
Alex-98jeweler <zubakin.alexandr98@gmail.com>
2023-02-16 12:11:08 +0700

    create item model and make first migration

--- items/models.py
+++ items/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 
+class Items(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048)
+    price = models.IntegerField()
