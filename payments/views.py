42a972f96a2a6e0134f827ec24e642159891d1f4
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-02-20 04:33:02 +0700

    edit payments view

--- payments/views.py
+++ payments/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
+from django.http import HttpRequest
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import stripe
+import re
 
 from items.models import Items
 
@@ -10,13 +12,17 @@ from items.models import Items
 stripe.api_key = settings.STRIPE_SECRET
 
 @api_view(("GET", ))
-def checkout_session(request, item_id):
+def checkout_session(request: HttpRequest, item_id):
     item = Items.objects.get(pk=item_id)
+    url = request.build_absolute_uri()
+    pattern = "buy/\d+/"
+    url = re.sub(pattern, '', url)
+    print(url)
     session = stripe.checkout.Session.create(
         line_items=[
             {
             "price_data": {
-                "currency": "rub",
+                "currency": "usd",
                 "product_data": {"name": item.name},
                 "unit_amount": item.price,
             },
@@ -24,8 +30,8 @@ def checkout_session(request, item_id):
             },
         ],
         mode="payment",
-        success_url='http://localhost:8000/success',
-        cancel_url='http://localhost:8000/cancel',
+        success_url=f'{url}success',
+        cancel_url=f'{url}cancel',
     )
     return Response(session.to_dict())
     
