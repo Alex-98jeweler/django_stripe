17566339ea49f3c2560fa1868dd9e4f80e91a68c
Alex-98jeweler <zubakin.alexandr98@gmail.com>
2023-02-20 12:55:32 +0700

    edit db settings and ad environment vars

--- django_stripe/settings.py
+++ django_stripe/settings.py
@@ -80,14 +80,11 @@ WSGI_APPLICATION = 'django_stripe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -134,3 +131,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+STRIPE_PUBLISH_KEY = os.getenv("STRIPE_PUBLISH")
